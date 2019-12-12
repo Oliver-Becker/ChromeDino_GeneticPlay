1e85972fd4cd57aab3251a608f82c7aa164a9e38
RafaelFRoque <cupuacu@DESKTOP-E1TT0RR.localdomain>
2019-12-12 17:59:37 -0200

    Arquivos Iniciais

---
+++ src/ai/models/random/RandomModel.js
@@ -0,0 +1,61 @@
+import Model from '../Model';
+
+export default class RandomModel extends Model {
+  weights = [];
+  biases = [];
+
+  init() {
+    
+    this.randomize();
+  }
+
+  predict(inputXs) {
+    const inputX = inputXs[0];
+    const y =
+      this.weights[0] * inputX[0] +
+      this.weights[1] * inputX[1] +
+      this.weights[2] * inputX[2] +      
+      this.weights[3] * inputX[3] +
+      this.weights[4] * inputX[4] +
+      this.biases[0];
+
+
+      let aux;
+      if(y < -.1)
+        aux = 1;
+      else if(y < .3)
+        aux = 0;
+      else 
+        aux = -1;
+    return aux;
+  }
+
+  train() {
+    this.randomize();
+  }
+
+  randomize() {
+    this.weights[0] = random();
+    this.weights[1] = random();
+    this.weights[2] = random();
+    this.weights[3] = random();
+    this.weights[4] = random();
+    this.biases[0] = random();
+  }
+  getChromosome() {
+    return this.weights.concat(this.biases);
+  }
+
+  setChromosome(chromosome) {
+    this.weights[0] = chromosome[0];
+    this.weights[1] = chromosome[1];
+    this.weights[2] = chromosome[2];
+    this.weights[3] = chromosome[3];
+    this.weights[4] = chromosome[4];
+    this.biases[0] = chromosome[5];
+  }
+}
+
+function random() {
+  return (Math.random() - .5) * 2;
+}
---
+++ src/apps/genetic.js
@@ -0,0 +1,89 @@
+import 'babel-polyfill';
+import { CANVAS_WIDTH, CANVAS_HEIGHT } from '../game/constants';
+import { Runner } from '../game';
+import GeneticModel from '../ai/models/genetic/GeneticModel';
+import RandomModel from '../ai/models/random/RandomModel';
+
+// const DINO_COUNT = 10;
+
+let runner = null;
+
+const rankList = [];
+const geneticModel = new GeneticModel();
+
+let firstTime = true;
+
+function setup() {
+  // Initialize the game Runner.
+  runner = new Runner('.game', {
+    DINO_COUNT:10,
+    onReset: handleReset,
+    onCrash: handleCrash,
+    onRunning: handleRunning
+  });
+  // Set runner as a global variable if you need runtime debugging.
+  window.runner = runner;
+  // console.info(runner)
+  // Initialize everything in the game and start the game.
+  runner.init();
+}
+
+
+function handleReset(Dinos) {
+  if (firstTime) {
+    firstTime = false;
+    // console.info("in here")
+    // console.info(Dinos)
+    Dinos.forEach((dino) => {
+      // console.info("happened");
+      dino.model = new RandomModel();
+      dino.model.init();
+    });
+
+  }
+  else {
+    // Train the model before restarting.
+    console.info('Training');
+    const chromosomes = rankList.map((dino) => dino.model.getChromosome());
+    // console.info(chromosomes)
+    // Clear rankList
+    rankList.splice(0);
+    geneticModel.fit(chromosomes);
+    Dinos.forEach((dino, i) => {
+      dino.model.setChromosome(chromosomes[i]);
+    });
+  }
+}
+
+function handleRunning(dino, state) {
+  let action = 0;
+  if (!dino.jumping) {
+    action = dino.model.predictSingle(convertStateToVector(state));
+  }
+  return action;
+}
+
+function handleCrash(dino) {
+   console.info("i was called")
+  if (!rankList.includes(dino)) {
+    rankList.unshift(dino);
+  }
+}
+
+function convertStateToVector(state) {
+  if (state) {
+    let a = 0;
+    if(state.obstacleType === 'PTERODACTYL')
+      a = 1;
+    return [
+      state.obstacleX / CANVAS_WIDTH,
+      (CANVAS_HEIGHT - state.obstacleY)/CANVAS_HEIGHT,
+      state.obstacleWidth / CANVAS_WIDTH,
+      state.speed / 100,
+      a
+    ];
+  }
+  return [0, 0, 0, 0, 0];
+}
+
+document.addEventListener('DOMContentLoaded', setup);
